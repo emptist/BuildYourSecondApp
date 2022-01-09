d69a4495169a62a96448c38917142983cffcb213
emptist <jigme1968@gmail.com>
2022-01-09 10:48:18 +0800

    Initial Commit

---
+++ BuildYourSecondApp/BuildYourSecondAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildYourSecondAppApp.swift
+//  BuildYourSecondApp
+//
+//  Created by jk on 2022/1/9.
+//
+
+import SwiftUI
+
+@main
+struct BuildYourSecondAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BuildYourSecondApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BuildYourSecondApp
+//
+//  Created by jk on 2022/1/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
